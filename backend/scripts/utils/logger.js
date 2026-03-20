f12f837f8d72e73b463957bdf0c5dec9e386e8f5
MyD <m.demir-21@outlook.com>
2026-03-21 02:18:44 +0300

    Update logger.js

--- backend/scripts/utils/logger.js
+++ backend/scripts/utils/logger.js
@@ -13,7 +13,7 @@ const logger = createLogger({
   
   format: format.combine(
     format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-    [span_4](start_span)format.errors({ stack: true }), // Hata yığınlarını (stack trace) yakalar[span_4](end_span)
+    format.errors({ stack: true }), // Hata yığınlarını (stack trace) yakalar
     format.json(), // Dosyada yapılandırılmış veri tutar
     format.printf(({ timestamp, level, message, stack, ...meta }) => {
       // .txt dosyasında okunabilir format
